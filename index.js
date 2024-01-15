7cf5f03bb6af6294e837e52b42d173dad2dfbc52
KevinDsouza7 <victorkevindsouza7@gmail.com>
2024-01-16 01:33:38 +0400

    code reconstruct

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+function generateREADME(data) {
+    return `
+# ${data.title}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license. ![License Badge](https://img.shields.io/badge/license-${data.license}-green)
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For questions or concerns, please contact ${data.email}. You can also visit [my GitHub profile](https://github.com/${data.username}).
+`;
+}
+
 // array of questions for user
 const questions = [
     { type: 'input', name: 'title', message: 'Enter the title of your project:' },
@@ -24,7 +60,7 @@ const questions = [
 // function to write README file
 function writeToFile(fileName, data) {
     const outputPath = path.join(__dirname, fileName);
-    const readmeContent = generateMarkdown(data);
+    const readmeContent = generateREADME(data);
 
     fs.writeFile(outputPath, readmeContent, (err) => {
         if (err) {
